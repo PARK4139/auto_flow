6877b9fc31f3bbc208eca764705ff09e019d34fd
jung hoon park <wjdgns4139@gmail.com>
2025-11-19 17:07:57 +0900

    feat: make initial structure

--- source/constants/project_paths.py
+++ source/constants/project_paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 D_ROOT =  Path(__file__).parent.parent
-D_FUNCTIONS = Path(D_ROOT) / "source" / "functions"
-D_WRAPPERS = Path(D_ROOT) / "source" / "wrappers"
+D_FUNCTIONS = D_ROOT / "functions"
+D_WRAPPERS = D_FUNCTIONS
